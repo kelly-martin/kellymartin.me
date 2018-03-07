24347bbef9210a3168d4e35171d8f0e7928a269c
kellymartin.me@gmail.com <kellymartin.me@gmail.com>
2018-03-07 14:51:11 -0700

    updates to about me and portfolio

--- about.php
+++ about.php
@@ -6,65 +6,38 @@
 
   <div class="container body">
     <center><h2>About Me</h2></center>
-    <div class="row">
-      <div id="headshotDiv"><img id="headshot2" src="files/headshot2.jpg"></div>
-      <div id="quickFacts">
-        <p>I live in Denver, CO. <img tabindex="0" class="popoverIcon" src="files/popover.svg"
-          data-toggle="popover" data-trigger="focus" data-content="Being near the Rocky Mountains has made me
-          fall in love with hiking and backpacking. I also love to run and bike."></p>
-        <p>I'm a minimalist. <img tabindex="0" class="popoverIcon" src="files/popover.svg"
-          data-toggle="popover" data-trigger="focus" data-content="I have about 60 total
-          possessions, from clothes to kitchenware, from big to small, from functional to whimsical."></p>
+    <div id="aboutMeRow" class="row">
+      <div id="headshotDiv" class="col-lg-5"><img id="headshot2" src="files/headshot2.jpg"></div>
+      <div id="quickFacts" class="col-lg-7">
+        <p>I spent the first 18 years of my life in Cincinnati, Ohio. <img tabindex="0" class="popoverIcon" src="files/popover.svg"
+          data-toggle="popover" data-trigger="focus" data-html="true" data-content="A city known for its
+          <a class='popoverLink' href='http://cincinnatibrewinghistory.com/' target='_blank'>beer history</a>,
+          <a class='popoverLink' href='https://en.wikipedia.org/wiki/Skyline_Chili' target='_blank'>unique chili</a>, and
+          <a class='popoverLink' href='https://en.wikipedia.org/wiki/Graeter%27s' target='_blank'>Oprah-worthy ice cream</a>,
+          Cincy's got a lot to offer."></p>
+          <p>I attended <a href="http://www.espn.com/video/clip?id=18441710" target="_blank">Clemson University</a>
+            and graduated with a degree in Computer Information Systems in May 2016.
+            <img tabindex="0" class="popoverIcon" src="files/popover.svg" data-toggle="popover" data-trigger="focus"
+            data-content="Computer Information Systems is a Bachelor's of Science major offered in the School of Computing.
+            It consists of core Computer Science classes along with electives in Management Information Systems."></p>
+        <p>I moved to Denver in July 2016, and have fallen in love with Colorado.
+          <img tabindex="0" class="popoverIcon" src="files/popover.svg" data-toggle="popover" data-trigger="focus"
+          data-content="I don't think I'll ever leave the Rocky Mountain State.
+          The people, beer, music, weather, and wilderness are way too amazing."></p>
+        <p>I love the outdoors; my favorite activities include: backpacking, hiking, snowboarding, biking, and trail running.
+          <img tabindex="0" class="popoverIcon" src="files/popover.svg" data-toggle="popover" data-trigger="focus"
+          data-content="Fun fact: I skiied all season during my first winter here (2017) and hated it. I had accepted that I
+          wouldn't be able to take part in the snow sports. On a whim, I took a snowboarding lesson in February 2018 and fell in
+          love with it immediately. I'm so glad to now be able to appreciate the mountains all year round."></p>
         <p>I'm obsessed with coffee. <img tabindex="0" class="popoverIcon" src="files/popover.svg"
           data-toggle="popover" data-html="true" data-trigger="focus" data-content="One could say I'm a <em>caff-fiend</em>.
-          <br>Check out <a class='popoverLink' href='/portfolio/mochaMap' target='_blank'>this cool coffee-related web app</a> I made."></p>
+          <br>I was loyal to the French Press for quite a while but I recently switched to a Bialetti, and I'm never going back.
+          <br>P.S. Check out <a class='popoverLink' href='/portfolio/mochaMap' target='_blank'>this cool coffee-related web app</a> I made."></p>
         <p>I love to create with code. <img tabindex="0" class="popoverIcon" src="files/popover.svg"
-          data-toggle="popover" data-html="true" data-trigger="focus" data-content="Check out my web development skills <a class='popoverLink' href='skills'>here</a> or explore my <a class='popoverLink' href='portfolio'>portfolio</a>."></p>
+          data-toggle="popover" data-html="true" data-trigger="focus"
+          data-content="Check out my <a class='popoverLink' href='https://kellymartin.me/files/MartinKellyResume.pdf' target='_blank'>resume</a> or explore my <a class='popoverLink' href='portfolio'>portfolio</a>."></p>
       </div>
     </div>
-    <hr>
-    <h4>Where I've Been, What I've Done</h4>
-    <p>
-      I was born and raised in Cincinnati, Ohio. A city known for its
-      <a href="http://cincinnatibrewinghistory.com/" target="_blank">beer history</a>,
-      <a href="https://en.wikipedia.org/wiki/Skyline_Chili" target="_blank">unique chili</a>, and
-      <a href="https://en.wikipedia.org/wiki/Graeter%27s" target="_blank">Oprah-worthy ice cream</a>,
-      Cincy's got a lot to offer. After spending the first 18 years
-      of my life there, however, I was ready for a change. In 2012, I headed
-      south for college, enticed by warm weather and football.
-    </p>
-    <p>
-      I attended
-      <a href="http://www.espn.com/video/clip?id=18441710" target="_blank">Clemson University</a>
-      and graduated in May 2016 with a degree in Computer Information Systems
-      (essentially, a combination of core Computer Science classes and electives in
-      Management Information Systems). I completed a couple tech internships during
-      my summers while at Clemson - one in the healthcare industry on the east coast,
-      the other in aerospace on the west coast. These experiences gave me valuable
-      insight into the kind of work I wanted to do as well as where I wanted to live.
-    </p>
-
-    <p>
-      I accepted a job with a large telecommunications company during my senior
-      year and moved to Denver, CO in July 2016. I worked at that company as a
-      <a href="https://www.mountaingoatsoftware.com/agile/scrum/roles/product-owner" target="_blank">product owner</a>
-      for a little over a year. I had a lot of responsiblity, an awesome
-      team environment, and worked on a lot of high-visibility projects. I feel very lucky
-      and grateful to have had such a positive experience for my first full-time job
-      after college. However, I was not entirely intrigued by the growth opportunities
-      and the future paths available for a product owner.
-    </p>
-    <p>
-      I knew I wanted a change but I didn't know exactly what kind of change.
-      So, in August 2017, I paid off the remainder of my student loans and left
-      DISH to answer the age-old question: “What am I going to do with my life?”.
-      Since then, I have traveled; I have taught myself a variety of web
-      languages/frameworks and become passionate about full-stack web
-      development; and, most importantly, I have developed a clearer understanding
-      of what I find to be meaningful work. Currently, I am applying for jobs at
-      tech companies in the Denver metro area. If you are an employer, feel free
-      to <a href="skills">check out my skills</a> or <a href="portfolio">explore my portfolio</a>.
-    </p>
   </div>
 
 <?php
--- portfolio/index.php
+++ portfolio/index.php
@@ -7,6 +7,14 @@
   <div class="container body">
     <div class="row">
       <div class="col-sm-4">
+        <a href="https://skycast-app.herokuapp.com/" target="_blank"><img class="portfolioThumbnail" src="../files/skyCast.png"></a>
+        <br><br><center><strong><p><a href="https://skycast-app.herokuapp.com/" target="_blank">SkyCast</a></p></strong></center>
+        <p>A weather forecast app that uses the Google Places, the Google Geocoding, and the Dark Sky API to show
+          current, future, and historical weather data for a user-searched location.
+          <br>This app is hosted on Heroku and is comprised of a ReactJS UI running on a NodeJS server with ExpressJS.
+          <br>View the code in my <a href="https://github.com/kelly-martin/skyCast" target="_blank">@skyCast</a> repo on GitHub.</p>
+      </div>
+      <div class="col-sm-4">
         <a href="mochaMap" target="_blank"><img class="portfolioThumbnail" src="../files/mochaMap.png"></a>
         <br><br><center><strong><p><a href="mochaMap" target="_blank">MochaMap</a></p></strong></center>
         <p>Keep track of your coffee shops: the ones you've visited, your favorites, and those on your
@@ -16,16 +24,6 @@
           database. AJAX is used to call these PHP scripts; any data returned is in XML format and is parsed
           before use.<br>
           View the code in my <a href="https://github.com/kelly-martin/mochaMap" target="_blank">@mochaMap</a> repo on GitHub.</p>
-      </div>
-      <div class="col-sm-4">
-        <a href="secretDiary/diary.php" target="_blank"><img class="portfolioThumbnail" src="../files/secretDiary.png"></a>
-        <br><br><center><strong><p><a href="secretDiary/diary.php" target="_blank">Secret Diary</a></p></strong></center>
-        <p>With serene imagery to calm you and a secure login system to give you peace of mind, this online diary
-          is the perfect place to let your deepest thoughts flow freely.
-          <br>I built this app as an exercise in implementing form validation and a login/registration system with PHP and
-          jQuery. User login information and their diary entry is stored in a MySQL database table. AJAX is used to update
-          their diary entry in the database without the need for a page load.<br>
-          View the code in my <a href="https://github.com/kelly-martin/secretDiary" target="_blank">@secretDiary</a> repo on GitHub.</p>
       </div>
       <div class="col-sm-4">
         <a href="codePlayer" target="_blank"><img class="portfolioThumbnail" src="../files/codePlayer.png"></a>
